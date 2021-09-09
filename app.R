2e9cb903d1be11b331b9ef545184017a212c02a5
ZekeMarshall <60969328+ZekeMarshall@users.noreply.github.com>
2021-09-09 09:23:28 +0100

    Basic normal curve for AT

--- app.R
+++ app.R
@@ -8,26 +8,47 @@
 #
 
 library(shiny)
+library(ggplot2)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("ESC-DSS Function Generator"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            selectInput(
+                inputId = "suit_factor",
+                label = "Suitability Factor:",
+                choices = c("Accumulated Temperature" = "at",
+                            "Continentality" = "ct",
+                            "Exposure" = "dams",
+                            "Moisture Deficit" = "md",
+                            "Soil Moisture Regime" = "smr",
+                            "Soil Nutrient Regime" = "snr")
+            ),
+            selectInput(
+                inputId = "function_type",
+                label = "Function:",
+                choices = c("Normal Distribution" = "norm")
+            ),
+            sliderInput("mean",
+                        "Mean:",
+                        min = 0,
+                        max = 320,
+                        value = 160),
+            sliderInput("sd",
+                        "Spread:",
+                        min = 0,
+                        max = 100,
+                        value = 15)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("suitPlot")
         )
     )
 )
@@ -35,13 +56,78 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    output$suitPlot <- renderPlot({
+        
+        if(input$function_type == "norm"){
+            
+            mean <- input$mean
+            sd <- input$sd
+            
+            at_x_vec <- c(0:320)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+            n <- 155
+
+            res <- dnorm(x =  at_x_vec, mean = as.numeric(mean), sd = as.numeric(sd))
+            ymax <- max(res)
+
+            # ***Change these to actual quartile values from res***
+            ymax_1q <- ymax*0.25
+            # ymax_1qy <- qnorm(.25, mean = 150, sd = 15)
+            ymax_2q <- ymax*0.5
+            ymax_3q <- ymax*0.75
+            
+            # Find points where ymax is above res
+            above_1q <- ymax_1q > res
+            above_2q <- ymax_2q > res
+            above_3q <- ymax_3q > res
+            
+            # Points always intersect when above=TRUE, then FALSE or reverse
+            intersect_points_1q <- which(diff(above_1q) != 0)
+            intersect_points_2q <- which(diff(above_2q) != 0)
+            intersect_points_3q <- which(diff(above_3q) != 0)
+            
+    
+            p1 <- ggplot2::ggplot(data = data.frame(x = c(0, 320)), aes(x)) +
+                ggplot2::stat_function(fun = dnorm, n = 155, args = list(mean = mean, sd = sd)) +
+                ggplot2::geom_hline(yintercept = ymax_1q,
+                                    size = 0.5,
+                                    color = "grey") +
+                ggplot2::geom_vline(xintercept = intersect_points_1q[1],
+                                    size = 0.5,
+                                    color = "grey") +
+                ggplot2::geom_vline(xintercept = intersect_points_1q[2],
+                                    size = 0.5,
+                                    color = "grey") +
+                ggplot2::geom_hline(yintercept = ymax_2q,
+                                    size = 0.5,
+                                    color = "grey") +
+                ggplot2::geom_vline(xintercept = intersect_points_2q[1],
+                                    size = 0.5,
+                                    color = "grey") +
+                ggplot2::geom_vline(xintercept = intersect_points_2q[2],
+                                    size = 0.5,
+                                    color = "grey") +
+                ggplot2::geom_hline(yintercept = ymax_3q,
+                                    size = 0.5,
+                                    color = "grey") +
+                ggplot2::geom_vline(xintercept = intersect_points_3q[1],
+                                    size = 0.5,
+                                    color = "grey") +
+                ggplot2::geom_vline(xintercept = intersect_points_3q[2],
+                                    size = 0.5,
+                                    color = "grey") +
+                ggplot2::theme_classic() +
+                ggplot2::coord_cartesian(xlim = c(0, 320),
+                                         expand = FALSE) +
+                NULL
+                
+            
+            
+            p1
+        
+        }
+        
+        
     })
 }
 
